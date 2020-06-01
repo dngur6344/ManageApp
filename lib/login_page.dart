de69ef5614950207522c42c9c7a057e344f4800d
dngur6344 <dngur6344@gmail.com>
2020-06-01 23:52:22 +0900

    Using RestAPI Get Method

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -13,11 +15,26 @@ class _LoginPageState extends State<LoginPage> {
 
   void validateAndSave() {
     final form = formKey.currentState;
+    Future<Post> post = fetchPost();
+
     if (form.validate()) {
       form.save();
       print('Form is valid Email: $_email, password: $_password');
     } else {
       print('Form is invalid Email: $_email, password: $_password');
+    }
+  }
+
+  Future<Post> fetchPost() async {
+    final response = await get('http://localhost:8080/teachers/$_email');
+    if (response.statusCode == 200) {
+      // 만약 서버가 OK 응답을 반환하면, JSON을 파싱합니다.
+
+      print(Post.fromJson(json.decode(response.body)).tel);
+      return Post.fromJson(json.decode(response.body));
+    } else {
+      // 만약 응답이 OK가 아니면, 에러를 던집니다.
+      throw Exception('Failed to load post');
     }
   }
 
@@ -58,6 +75,23 @@ class _LoginPageState extends State<LoginPage> {
           ),
         ),
       ),
+    );
+  }
+}
+class Post {
+  final String teacherid;
+  final String name;
+  final String tel;
+  final String birthday;
+
+  Post({this.teacherid, this.name, this.tel, this.birthday});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      teacherid: json['teacher_id'],
+      name: json['name'],
+      tel: json['tel'],
+      birthday: json['birthday'],
     );
   }
 }
